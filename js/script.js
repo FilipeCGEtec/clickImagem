16b9beda49bdc89268dbba13f2940aacdd88b488
DreiJD <127852282+FilipeCGEtec@users.noreply.github.com>
2023-03-14 15:09:55 -0300

    Update script.js
    script js

---
+++ js/script.js
@@ -0,0 +1,10 @@
+var sotc = document.getElementById('imagem')
+var img
+
+function numeroImg(min, max){
+  img = math.floor(math.random() * (max - min + 1) + 1
+};
+sotc.addEventListener('click', () =>{
+  numeroImg(1, 16)
+  sotc.setAttribute("src", "img/+img+".png");
+});
