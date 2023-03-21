cfce3ceb4dc2c4d7ebe096f2e338e02dead7ed4b
DreiJD <127852282+FilipeCGEtec@users.noreply.github.com>
2023-03-21 11:15:21 -0300

    Correção do script
    correção da função "Math"

--- js/script.js
+++ js/script.js
@@ -2,10 +2,10 @@ var sotc = document.getElementById('imagem')
 var img
 
 function numeroImg(min, max){
-  img = math.floor(math.random() * (max - min + 1)) + 1
+  img = Math.floor(Math.random() * (max - min + 1)) + 1;
 };
 sotc.addEventListener('click', () =>{
   numeroImg(1, 16)
-  sotc.setAttribute("src", "img/"+img+".png");
+  sotc.setAttribute("src", "img/"+ img +".png");
   console.log(img);
 });
